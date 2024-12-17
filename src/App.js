8208d65c8e8af256d3fb095de47d361792805154
Mohit <swtmohit2002@gmail.com>
2024-12-17 10:30:15 +0530

    change me

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
        <h1>hello world siddharth</h1>
+       <h1>hello world Mohit</h1>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
